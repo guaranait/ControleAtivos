244b9a5088c552291f97b6b881fb5b7599b9b1f6
jeanbellio <jean.bellio@acad.pucrs.br>
2018-05-07 19:58:27 -0300

    funcionario e interface para dashboard

---
+++ Backend/Restful/src/br/com/restful/dao/DashboardDAO.java
@@ -0,0 +1,76 @@
+package br.com.restful.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import br.com.restful.factory.ConnectionFactory;
+import br.com.restful.model.Ativo;
+
+public class DashboardDAO {
+
+	private static DashboardDAO instance;
+
+	public static DashboardDAO getInstance() {
+		if (instance == null)
+			instance = new DashboardDAO();
+		return instance;
+	}
+
+	public ArrayList<HashMap> contadorAtivos() {
+		//ArrayList<Ativo> ativos = null;
+
+		// teste
+		/*ativos = new ArrayList<Ativo>();
+		Ativo ativo = new Ativo();
+		ativo.setId(01);
+		ativo.setDescricao("MacBook Air 13'");
+		ativo.setDtCompra(new Date());
+		ativo.setFabrincante("Apple");
+		ativo.setVlCompra(3900.00);
+		ativo.setVlDepreciado(800.00);
+		ativos.add(ativo);*/
+
+		String sql = "SELECT s.descricao descricao, count(a.id) total FROM lu2cas01.ATIVO a, lu2cas01.STATUS s WHERE a.id_status = s.id GROUP BY s.descricao";
+		 
+		ArrayList<HashMap> contadorAtivos = new ArrayList<>();
+		 
+		Connection conn = null;
+		PreparedStatement pstm = null;
+		ResultSet rs = null;
+		 
+		try {
+			conn = ConnectionFactory.criarConexao();
+			pstm = conn.prepareStatement(sql);
+			rs = pstm.executeQuery();
+		 
+			while(rs.next()){
+				HashMap<String, Long> contador = new HashMap<>();
+				 
+				contador.put(rs.getString("descricao"), rs.getLong("total"));
+
+				contadorAtivos.add(contador);
+			}
+		 } catch (Exception e) {
+			 e.printStackTrace();
+		 }finally{
+			 try{
+				 if(rs != null){
+					 rs.close();
+				 }
+				 if(pstm != null){
+					 pstm.close();
+				 }
+				 if(conn != null){
+					 conn.close();
+				 }
+			 }catch(Exception e){
+				 e.printStackTrace();
+			 }
+		 }
+		return contadorAtivos;
+	}
+
+}
---
+++ Backend/Restful/src/br/com/restful/dao/FuncionarioDAO.java
@@ -0,0 +1,267 @@
+package br.com.restful.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.sql.Date;
+
+import br.com.restful.factory.ConnectionFactory;
+import br.com.restful.model.Funcionario;
+
+public class FuncionarioDAO {
+
+	private static FuncionarioDAO instance;
+
+	public static FuncionarioDAO getInstance() {
+		if (instance == null)
+			instance = new FuncionarioDAO();
+		return instance;
+	}
+
+	public ArrayList<Funcionario> listarFuncionarios() {
+		//ArrayList<Funcionario> funcionarios = null;
+
+		// teste
+		/*funcionarios = new ArrayList<Funcionario>();
+		Funcionario funcionario = new Funcionario();
+		funcionario.setId(01);
+		funcionario.setDescricao("MacBook Air 13'");
+		funcionario.setDtCompra(new Date());
+		funcionario.setFabrincante("Apple");
+		funcionario.setVlCompra(3900.00);
+		funcionario.setVlDepreciado(800.00);
+		funcionarios.add(funcionario);*/
+
+		String sql = "SELECT * FROM lu2cas01.FUNCIONARIO";
+		 
+		ArrayList<Funcionario> funcionarios = new ArrayList<Funcionario>();
+		 
+		Connection conn = null;
+		PreparedStatement pstm = null;
+		ResultSet rs = null;
+		 
+		try {
+			conn = ConnectionFactory.criarConexao();
+			pstm = conn.prepareStatement(sql);
+			rs = pstm.executeQuery();
+		 
+			while(rs.next()){
+				Funcionario funcionario = new Funcionario();
+				 
+				/*funcionario.setId(rs.getLong("id"));
+				funcionario.setDescricao(rs.getString("descricao")); 
+				funcionario.setDtCompra(rs.getDate("dt_compra"));
+				funcionario.setFabricante(rs.getString("fabricante"));
+				funcionario.setVlCompra(rs.getDouble("vl_compra"));
+				funcionario.setVlDepreciado(rs.getDouble("vl_depreciado"));
+				funcionario.setCriadoPor(rs.getLong("criado_por"));
+				funcionario.setDtCriacao(rs.getDate("dt_criacao"));
+				funcionario.setIdStatus(rs.getLong("id_status"));
+				funcionario.setModificadoPor(rs.getLong("modificado_por"));
+				funcionario.setDtModificacao(rs.getDate("dt_modificacao"));*/
+				 
+				funcionarios.add(funcionario);
+			}
+		 } catch (Exception e) {
+			 e.printStackTrace();
+		 }finally{
+			 try{
+				 if(rs != null){
+					 rs.close();
+				 }
+				 if(pstm != null){
+					 pstm.close();
+				 }
+				 if(conn != null){
+					 conn.close();
+				 }
+			 }catch(Exception e){
+				 e.printStackTrace();
+			 }
+		 }
+		return funcionarios;
+	}
+
+	public Funcionario listarFuncionario(long id) {
+		/*
+		 * Connection conexao = null; PreparedStatement pstmt = null; ResultSet
+		 * rs = null;
+		 */
+
+		// teste
+		/*Funcionario funcionario = new Funcionario();
+		funcionario.setId(01);
+		funcionario.setDescricao("MacBook Air 13'");
+		funcionario.setDtCompra(new Date());
+		funcionario.setFabrincante("Apple");
+		funcionario.setVlCompra(3900.00);
+		funcionario.setVlDepreciado(800.00);*/
+		
+		Funcionario funcionario = null;
+		
+		String sql = "SELECT * FROM lu2cas01.ATIVO WHERE id = ?";
+		 
+		Connection conn = null;
+		PreparedStatement pstm = null;
+		ResultSet rs = null;
+		 
+		try {
+			conn = ConnectionFactory.criarConexao();
+			pstm = conn.prepareStatement(sql);
+			
+			pstm.setLong(1, id);
+			
+			rs = pstm.executeQuery();
+		 
+			if(rs.next()){
+				 
+				/*funcionario.setId(rs.getLong("id"));
+				funcionario.setDescricao(rs.getString("descricao")); 
+				funcionario.setDtCompra(rs.getDate("dt_compra"));
+				funcionario.setFabricante(rs.getString("fabricante"));
+				funcionario.setVlCompra(rs.getDouble("vl_compra"));
+				funcionario.setVlDepreciado(rs.getDouble("vl_depreciado"));
+				funcionario.setCriadoPor(rs.getLong("criado_por"));
+				funcionario.setDtCriacao(rs.getDate("dt_criacao"));
+				funcionario.setIdStatus(rs.getLong("id_status"));
+				funcionario.setModificadoPor(rs.getLong("modificado_por"));
+				funcionario.setDtModificacao(rs.getDate("dt_modificacao"));*/
+			}
+		 } catch (Exception e) {
+			 e.printStackTrace();
+		 }finally{
+			 try{
+				 if(rs != null){
+					 rs.close();
+				 }
+				 if(pstm != null){
+					 pstm.close();
+				 }
+				 if(conn != null){
+					 conn.close();
+				 }
+			 }catch(Exception e){
+				 e.printStackTrace();
+			 }
+		 }
+
+		return funcionario;
+	}
+
+	public Boolean cadastrarFuncionario(Funcionario funcionario) {
+		String sql = "INSERT INTO lu2cas01.FUNCIONARIO(nome,cpf,cnpj,cargo,criado_por,dt_criacao)"
+				+ " VALUES(?,?,?,?,?,?)";
+		
+		Connection conn = null;
+		PreparedStatement pstm = null;
+		boolean cadastrou = false;
+		try {
+			conn = ConnectionFactory.criarConexao();
+			pstm = conn.prepareStatement(sql);
+
+			pstm.setString(1, funcionario.getNome());
+			pstm.setString(2, funcionario.getCpf());
+			pstm.setString(3, funcionario.getCnpj());
+			pstm.setString(4, funcionario.getCargo());
+			pstm.setLong(5, funcionario.getCriadoPor());
+			pstm.setDate(6, new java.sql.Date(System.currentTimeMillis()));
+			
+			// Executa a sql para inserção dos dados
+			if(pstm.execute()){
+				cadastrou = true;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// Fecha as conexões
+			try {
+				if (pstm != null) {
+					pstm.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return cadastrou;
+	}
+
+	public Boolean alterarFuncionario(Funcionario funcionario) {
+		String sql = "UPDATE lu2cas01.FUNCIONARIO SET nome = ?,cpf = ?,cnpj = ?,cargo = ?,modificado_por = ?,dt_modificacao = ? WHERE id = ?";
+
+		Connection conn = null;
+		PreparedStatement pstm = null;
+		boolean alterou = false;
+		try {
+			conn = ConnectionFactory.criarConexao();
+			pstm = conn.prepareStatement(sql);
+			
+			pstm.setString(1, funcionario.getNome());
+			pstm.setString(2, funcionario.getCpf());
+			pstm.setString(3, funcionario.getCnpj());
+			pstm.setString(4, funcionario.getCargo());
+			pstm.setLong(5, funcionario.getModificadoPor());
+			pstm.setDate(6, new java.sql.Date(System.currentTimeMillis()));
+			pstm.setLong(7, funcionario.getId());
+
+			// Executa a sql para inserção dos dados
+			if(pstm.execute()){
+				alterou = true;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// Fecha as conexões
+			try {
+				if (pstm != null) {
+					pstm.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return alterou;
+	}
+
+	public boolean excluirFuncionario(long id) {
+		boolean sucesso = false;
+		String sql = "DELETE FROM lu2cas01.FUNCIONARIO WHERE id = ?";
+
+		Connection conn = null;
+		PreparedStatement pstm = null;
+
+		try {
+			conn = ConnectionFactory.criarConexao();
+			pstm = conn.prepareStatement(sql);
+
+			pstm.setLong(1, id);
+
+			// Executa a sql para inserção dos dados
+			if (pstm.execute()) {
+				sucesso = true;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// Fecha as conexões
+			try {
+				if (pstm != null) {
+					pstm.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return sucesso;
+	}
+
+}
