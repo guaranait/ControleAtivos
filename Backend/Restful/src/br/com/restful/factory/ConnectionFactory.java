c0e57c8254d007ea79e71f712b786fd82610b66c
Jean <jean.bellio@acad.pucrs.br>
2018-05-08 20:58:04 -0300

    Revert "Auto stash before revert of "funcionario e interface para dashboard""
    This reverts commit ae50d66bca5ca36181fa50a12cac555b4bde6ca8.

--- Backend/Restful/src/br/com/restful/factory/ConnectionFactory.java
+++ Backend/Restful/src/br/com/restful/factory/ConnectionFactory.java
@@ -6,14 +6,14 @@ import java.sql.DriverManager;
 public class ConnectionFactory {
  
    //Nome do usuário do mysql
-   private static final String USERNAME = "";
+   private static final String USERNAME = "lu2cas01";
  
    //Senha do mysql
-   private static final String PASSWORD = "";
+   private static final String PASSWORD = "guaranait123";
  
    //Dados de caminho, porta e nome da base de dados que irá ser feita a conexão
    //private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/agenda";
-   private static final String DATABASE_URL = "";
+   private static final String DATABASE_URL = "jdbc:mysql://mysql.lu2cas.com.br";
    /**
    * Cria uma conexão com o banco de dados MySQL utilizando o nome de usuário e senha fornecidos
    * @param username
