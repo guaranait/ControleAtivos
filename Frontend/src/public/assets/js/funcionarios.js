6b3ffb4f2b70ef5d0cf0d593feac8b1a35154787
lu2cas <luccas.carsil@gmail.com>
2018-06-13 21:09:42 -0300

    Correção das funcionalidades de editar e excluir funcionários.

--- Frontend/src/public/assets/js/funcionarios.js
+++ Frontend/src/public/assets/js/funcionarios.js
@@ -1,13 +1,15 @@
 angular.module("FuncionariosModulo", []).controller("FuncionariosController", function($scope, $http) {
     $scope.nome;
-    $scope.cpfCnpj;
+    $scope.cpf;
+    $scope.cnpj;
     $scope.cargo;
 
     $scope.criarFuncionario = function() {
         var objetoFuncionario = {};
 
         objetoFuncionario.nome = $scope.nome;
-        objetoFuncionario.cpfCnpj = $scope.cpfCnpj;
+        objetoFuncionario.cpf = $scope.cpf;
+        objetoFuncionario.cnpj = $scope.cnpj;
         objetoFuncionario.cargo = $scope.cargo;
 
         console.log(objetoFuncionario);
