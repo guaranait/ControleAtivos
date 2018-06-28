222bffafd78f9872c4e413b307d0572fa79e9943
yasserveleda <veledinha-a@hotmail.com>
2018-06-27 21:38:46 -0300

    servico getemprestimos

--- Frontend/src/features/emprestimos/emprestimo.controller.js
+++ Frontend/src/features/emprestimos/emprestimo.controller.js
@@ -1,15 +1,25 @@
 class EmprestimoController {
-	constructor() {
+	constructor(EmprestimoService) {
+		this.EmprestimoService = EmprestimoService;
 		this.emprestimos = [];
-		let obj = {}
+		/*let obj = {}
 		obj.funcionario = { nome: 'João da Silva' };
 		obj.ativo = { id: '1234', descricao: 'Macbook pro 12' };
 		obj.dataEmprestimo = '12/06/2018';
-		this.emprestimos.push(obj);
-		console.log(this.emprestimos);
+		this.emprestimos.push(obj);*/
+		//console.log(this.emprestimos);
+		this.getEmprestimos();
 	}
+
+	getEmprestimos() {
+    	this.EmprestimoService.getEmprestimos().then( response => { 
+    		this.emprestimos = response.data 
+    		console.log(response);
+    	}).catch( error => console.log(error) );
+    }
+
 
 }
 
-EmprestimoController.$inject = [];
+EmprestimoController.$inject = ['EmprestimoService'];
 export default EmprestimoController;
--- Frontend/src/features/emprestimos/emprestimo.service.js
+++ Frontend/src/features/emprestimos/emprestimo.service.js
@@ -2,6 +2,14 @@ class EmprestimoService {
 	constructor($http) {
 		this.$http = $http;
 	}
+
+	getEmprestimos() {
+		return this.$http({
+			url: 'http://localhost:8080/Restful/emprestimo/listarEmprestimos',
+			method: "GET"
+		})
+		.then( response => response ).catch( error => error );
+	}
 }
 
 EmprestimoService.$inject = ['$http'];
