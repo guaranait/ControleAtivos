9ff4975f76db531bdcd9a6d49bb0899d9d106af3
yasserveleda <veledinha-a@hotmail.com>
2018-07-02 21:04:45 -0300

    concluindo o servico de vincular e desvincular com select de funcionario e ativos

--- Frontend/src/features/emprestimos/emprestimo.controller.js
+++ Frontend/src/features/emprestimos/emprestimo.controller.js
@@ -1,36 +1,91 @@
 class EmprestimoController {
-	constructor(EmprestimoService) {
+	constructor(EmprestimoService, $state, HomeService, FuncionariosService) {
 		this.EmprestimoService = EmprestimoService;
 		this.emprestimos = [];
-		/*let obj = {}
-		obj.funcionario = { nome: 'João da Silva' };
-		obj.ativo = { id: '1234', descricao: 'Macbook pro 12' };
-		obj.dataEmprestimo = '12/06/2018';
-		this.emprestimos.push(obj);*/
-		//console.log(this.emprestimos);
-		this.getEmprestimos();
+        this.$state = $state;
+        this.HomeService = HomeService;
+        this.FuncionariosService = FuncionariosService;
+        this.ativos;
+        this.funcionarios;
+        this.dtEmprestimo;
+        this.funcionarioSelecionado;
+        this.ativoSelecionado;
+        this.objetoModal;
+        this.getEmprestimos();
+        this.getAtivos();
+        this.getFuncionarios();
 	}
+
+    getAtivos() {
+        this.HomeService.getAtivos().then( response => { 
+            if(response.status == 200){
+                this.ativos = response.data 
+            }
+        }).catch( error => console.log(error) );
+    }
+
+    getFuncionarios() {
+        this.FuncionariosService.getFuncionarios().then( response => { 
+            if(response.status == 200){
+                this.funcionarios = response.data 
+            }
+        }).catch( error => console.log(error) );
+    }
 
 	getEmprestimos() {
     	this.EmprestimoService.getEmprestimos().then( response => { 
-    		this.emprestimos = response.data 
-    		//console.log(response);
+            if(response.status == 200) {
+                this.emprestimos = response.data 
+            }
     	}).catch( error => console.log(error) );
     }
 
-    removerVinculo(obj) {
-    	let objDevolucao = {};
-    	objDevolucao.dtDevolucao = moment().format("YYYY-MM-DD HH:mm:ss");
-    	objDevolucao.ativo = {id: obj.ativo.id};
-    	objDevolucao.funcionario = {id: obj.funcionario.id};
-    	objDevolucao.modificadoPor = 9999;
+    vincular() {
+        this.selectAtivo;
+        this.selectFuncionario;
+        
+        let objetoEmprestimo = {
+            funcionario: this.funcionarioSelecionado,
+            ativo: this.ativoSelecionado,
+            dtEmprestimo: moment(this.dtEmprestimo).format("YYYY-MM-DD HH:mm:ss"),
+            //Não esta aceitando data nula
+            dtDevolucao: moment().format("YYYY-MM-DD HH:mm:ss"),//moment().format("YYYY-MM-DD HH:mm:ss"),
+            criadoPor: 1,
+            modificadoPor: 1,
+            dtCriacao: moment().format("YYYY-MM-DD HH:mm:ss"),
+            dtModificacao: moment().format("YYYY-MM-DD HH:mm:ss")
+        };
 
+        this.EmprestimoService.cadastrarEmprestimo(objetoEmprestimo).then( response => { 
+            if(response.status == 200) {
+                this.$state.go('emprestimo');
+            }
+        }).catch( error => console.log(error) );
+    }
+    
+    desvincular() {
+    	let objDevolucao = angular.copy(this.objetoModal);
+    	objDevolucao.dtDevolucao = moment().format("YYYY-MM-DD HH:mm:ss");
     	this.EmprestimoService.devolverEmprestimo(objDevolucao).then( response => { 
-    		console.log(response);
+            if(response.status == 200) {
+                this.getEmprestimos();
+            }
     	}).catch( error => console.log(error) );
+    }
+
+    goVincular() {
+        this.$state.go('adicionar-vinculo');
+    }
+
+    cancelar() {
+        this.$state.go('emprestimo');
+    }
+
+    viewRemove(obj) {
+        this.objetoModal = angular.copy(obj);
     }
 
 }
 
-EmprestimoController.$inject = ['EmprestimoService'];
+EmprestimoController.$inject = ['EmprestimoService', '$state', 'HomeService', 'FuncionariosService'];
 export default EmprestimoController;
--- Frontend/src/features/emprestimos/emprestimo.service.js
+++ Frontend/src/features/emprestimos/emprestimo.service.js
@@ -11,8 +11,16 @@ class EmprestimoService {
 		.then( response => response ).catch( error => error );
 	}
 
+	cadastrarEmprestimo(obj) {
+		return this.$http({
+			url: 'http://localhost:8080/Restful/emprestimo/cadastrarEmprestimo',
+			method: "POST",
+			data: obj
+		})
+		.then( response => response ).catch( error => error );
+	}
+
 	devolverEmprestimo(obj) {
-		console.log(obj);
 		return this.$http({
 			url: 'http://localhost:8080/Restful/emprestimo/devolverEmprestimo',
 			method: "POST",
